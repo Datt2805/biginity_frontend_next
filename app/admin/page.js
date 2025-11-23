85da73cdfc68f532c6ecffbaf393980c66719156
Datt2805 <bhattdatt88@gmail.com>
2025-11-23 21:43:17 +0530

    page change

---
+++ app/admin/page.js
@@ -0,0 +1,17 @@
+// app/teacher/page.jsx
+import React from 'react';
+import AdminPage from './adminPage.jsx';
+
+// Set up metadata if needed
+export const metadata = {
+  title: 'Admin Page',
+};
+
+// This is the main page component for the /teacher route
+const adminPage = () => {
+  return (
+    <AdminPage />
+  );
+};
+
+export default TeacherPage;
